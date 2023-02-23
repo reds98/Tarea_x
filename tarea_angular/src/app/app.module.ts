0178905a2a287f9d3f67226aae3e08cd1b2cc043
reds98 <sahid.rojas64@gmail.com>
2023-02-22 19:43:59 -0600

    Cree el componente tarjeta

--- tarea_angular/src/app/app.module.ts
+++ tarea_angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TarjetaComponent } from './tarjeta/tarjeta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TarjetaComponent
   ],
   imports: [
     BrowserModule,
